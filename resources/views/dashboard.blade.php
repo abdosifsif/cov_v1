838c1a75789a72044b11fbba973e190532b750ca
sif <abdosiffbs@gmail.com>
2023-06-01 12:08:21 +0100

    randoms

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,68 +9,100 @@
     <link rel="stylesheet" href="{{ asset('css/styleHeader.css') }}">
 </head>
 
-<body onclick="hidMenu()">
-    <header>
-        
-        <div>
-            <nav>
-                <a href="/" class="logo"> Covoiturage</a>
-                <ul class="principale">
-                    <li><a href="#">
-                            <div class="link-wrap">
-                                <img src="images/search-svgrepo-com (1).svg">
-                                <div>
-                                    <p> Recherche</p>
-                                </div>
-                            </div>
-                        </a></li>
-                    <li><a href="#">
-                            <div class="link-wrap">
-                                <img src="images/add-circle-svgrepo-com.svg">
-                                <div>
-                                    <p> Publier un trajet</p>
-                                </div>
-                            </div>
-                        </a></l>
-                </ul>
-                <img src="{{ app('App\Http\Controllers\UserController')->getUserPic() }}" class="user-pic" onclick="showMenu()">
-
-                <div class="sub-menu-wrap" id="subMenu">
-                    <div class="sub-menu">
-                        <a href="/login" class="sub-menu-links">
-                            <p>Vos trajet</p>
-                            <span>></span>
-                        </a>
-                        <hr>
-                        <a href="Registre" class="sub-menu-links">
-                            <p>Messages</p>
-                            <span>></span>
-                        </a>
-                        <hr>
-                        <a href="Registre" class="sub-menu-links">
-                            <p>Profil</p>
-                            <span>></span>
-                        </a>
-                        <hr>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a href="#" class="sub-menu-links" onclick="event.preventDefault(); this.closest('form').submit();">
-                                <p>Déconnexion</p>
+<body>
+    <header class="main-head">
+        <nav>
+            <h2 id="logo">Covoiturage</h2>
+            <ul>
+                <li><a href="/ajouter-trajet">Ajouter Trajet</a></li>
+                <li><a href="/recherche">Recherche</a></li>
+                <li>
+                    <img src="{{ app('App\Http\Controllers\UserController')->getUserPic() }}" class="user-pic" onclick="showMenu()">
+                    <div class="sub-menu-wrap" id="subMenu">
+                        <div class="sub-menu">
+                            <a href="Registre" class="sub-menu-links">
+                                <p>Profil</p>
+                                <span>></span>  
+                            </a>
+                            <hr>
+                            <a href="Registre" class="sub-menu-links">
+                                <p>Messages</p>
                                 <span>></span>
                             </a>
-                        </form>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </div>
-       
-    </header>
-    <article>
 
- 
-    </article>
-    
+                            <hr>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="#" class="sub-menu-links"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    <p>Déconnexion</p>
+                                    <span>></span>
+                                </a>
+                            </form>
+                            </form>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section class="hero">
+        <h1>Covoiturage</h1>
+        <h3>Réserver vos trajets à petits prix</h3>
+        <button>Chercher Trajet</button>
+    </section>
+    <section class="description">
+        <div class="mov hidden">
+            <h1 class="titre">Réservez facilement</h1>
+            <P> Réservez votre trajet en quelques clics avec notre application
+                de covoiturage facile à utiliser.
+                Grâce à notre technologie de pointe,
+                vous pouvez trouver rapidement un trajet près de chez vous,
+                sans tracas ni complications. Chez [nom de la plateforme de covoiturage],
+                nous rendons le covoiturage simple et accessible à tous. <br><br></P>
+        </div>
+        <div class="mov hidden">
+            <h1 class="titre">Vaste choix à petits prix</h1>
+            <p>Voyagez à petit prix avec notre plateforme de covoiturage.
+                Nous proposons une grande variété de destinations en bus ou
+                en covoiturage, à des tarifs compétitifs. Chez [nom de la
+                plateforme de covoiturage], nous sommes attachés à offrir
+                des trajets abordables pour tous les budgets. Réservez dès
+                maintenant et économisez sur vos déplacements.</p>
+        </div>
+        <div class="mov hidden">
+            <h1 class="titre">Voyagez en toute confiance</h1>
+            <p>
+                Voyagez en toute confiance avec notre plateforme de
+                covoiturage sécurisée. Nous vérifions les profils et
+                les identités de tous nos membres et partenaires de
+                covoiturage, pour que vous puissiez réserver votre
+                trajet en toute sérénité. Chez [nom de la plateforme de covoiturage],
+                nous sommes déterminés à offrir un service fiable et sécurisé pour
+                tous vos déplacements en covoiturage.</p>
+        </div>
+    </section>
+    <section class="description2">
+        <img src="images/driver.jpg" alt="">
+        <div id="conduire">
+            <h1>Où voulez-vous conduire ?</h1>
+            <h4>Faisons de ce voyage le moins cher de tous les temps.</h4>
+            <button><a id="route_add_link" href="/ajouter-trajet">Publier un trajet </a></button>
+        </div>
+    </section>
+    <footer>
+        <h1>Savoir plus!</h1>
+        <div class="ftr">
+            <div class="information hidden">
+                <h3> à propos de Nous</h3>
+                <p>Majda et Abdellah deux étudiants qui ont décidé de créer leur application de covoiturage </p>
+            </div>
+            <div class="information hidden">
+                <h3>notre contact</h3>
+                <p>majdaelfadil4@gmail.com</p>
+            </div>
+        </div>
+    </footer>
     <script type="text/javascript" src="{{ URL::asset('scripts/myscripts.js') }}"></script>
 </body>
 
